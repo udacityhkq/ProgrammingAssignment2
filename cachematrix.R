e7e431744dac85594a27d949feb4b0959df6e860
udacityhkq <hoakevinquach@gmail.com>
2016-11-21 13:18:05 -0800

    Update cachematrix.R
    added comments to what each function does

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
+#################################################
+##
+##  The function, makeVector creates a special "vector", which is really a 
+##  list containing a function to:
+##
+##  * set the value of the vector
+##  * get the value of the vector
+##  * set the value of the mean
+##  * get the value of the mean
+##
+#################################################
 makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
@@ -17,6 +28,14 @@ makeVector <- function(x = numeric()) {
 }
 
 ## Write a short comment describing this function               
+#################################################
+##
+## The following function calculates the mean of the special "vector" created with the above function. 
+## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the 
+## cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the 
+## mean in the cache via the setmean function.  
+## 
+#################################################
 cachemean <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getmean()
